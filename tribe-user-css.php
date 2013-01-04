6950f6d0428913552aa5c3deb0ecae4a7937beb5
Andy Fragen <andy@thefragens.com>
2013-01-03 18:28:30 -0800

    really fix direct loading error

--- tribe-user-css.php
+++ tribe-user-css.php
@@ -3,11 +3,10 @@ header('Content-type: text/css');
 $http = 'http';
 if( isset($_SERVER['HTTPS']) ) { $http .= "s"; }
 $domain_url = $http."://".$_SERVER['SERVER_NAME'];
-if ( !( isset($_GET['theme']) || isset($_GET['plugs']) || isset($_GET['user']) ) ) { return; }
 if ( isset($_GET['subdir']) ) { $domain_url .= "/" . $_GET['subdir']; }
 $theme_events_path = '/wp-content/themes/' . $_GET['theme'] ;
-$plugs = $_GET['plugs'];
-$user = $_GET['user'];
+isset($_GET['plugs']) ? $plugs = $_GET['plugs'] : $plugs = '1';
+isset($_GET['user']) ? $user = $_GET['user'] : $user = '1';
 $count = count($plugs);
 $css = array();
 for ($i = 0; $i < $count; $i++) {
